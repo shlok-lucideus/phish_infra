db1f681432fc37a5b0c20f7a0b51f1f8aa25a19e
luciafx <89973739+lucidafx@users.noreply.github.com>
2021-09-07 15:22:04 +0530

    Updated

--- setup.sh
+++ setup.sh
@@ -1,44 +1,21 @@
 #!/bin/bash
 
-dockerhub_username="name"
-github_repo_url="repo"
 phish_infra_dir=/opt/phish_infra
 
 packages="openssl python3-certbot-nginx python3-pip unzip tmux git docker docker.io docker-compose certbot net-tools iputils-ping iproute2 curl wget nano"
 
 function usage {
-
-    echo "Usage: $0 -m setup" >&2
-    echo "       $0 -m gophish -h example.org -s login admin blog" >&2
+    echo "Usage: $0 -m gophish -h example.org -s login admin blog" >&2
     echo "       $0 -m evilginx -h example.org -t info -s login admin blog" >&2
     echo "" >&2
     echo "    -m : mode" >&2
     echo "    -h : hostname" >&2
     echo "    -s : subdomains" >&2
     echo "    -t : tracking domain" >&2
-    exit 1
-    
+    exit 1    
 }
 
-
-function setup {
-
-    DEBIAN_FRONTEND=noninteractive apt-get -y update && apt-get -y dist-upgrade && apt-get install -y $packages && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
-    
-    for image in evilginx gophish nginx
-    do
-	    docker pull $dockerhub_username/$image
-    done
-    
-    git clone $github_repo_url $phish_infra_dir
-
-}
-
-gophish() {
-    
-    hostname=$1
-    subdomains=$2
-
+function calc_hosts {
     for i in ${!subdomains[@]}
     do
         hosts_here[i+1]=${subdomains[i]}.$hostname
@@ -46,50 +23,46 @@ gophish() {
     hosts_here[0]=$hostname
     printf -v joined '%s,' "${hosts_here[@]}"
     hosts_here=`echo "${joined%,}"`
-
-    cp $phish_infra_dir/docker-compose.template $phish_infra_dir/docker-compose.yml
-    sed -i "s/hosts_here/$hosts_here/g" $phish_infra_dir/docker-compose.yml
-
-    cd $phish_infra_dir
-    docker-compose up -d
-
 }
 
-evilginx() {
-    
+function gen_certs {
+    mkdir -p $phish_infra_dir/certs
+    cd $phish_infra_dir/certs
+    openssl genrsa 2048 > $hostname.key
+    openssl req -new -x509 -nodes -sha256 -days 365 -key $hostname.key -out $hostname.crt -subj "/C=US/ST=Oregon/L=Portland/CN=*.$hostname"
+}
+
+function compose_init {
+    cp $phish_infra_dir/docker-compose.template $phish_infra_dir/docker-compose.yml
+}
+
+function compose_up {
+    cd $phish_infra_dir
+    docker-compose up -d    
+}
+
+gophish() { 
+    hostname=$1
+    subdomains=$2
+    gen_certs
+    calc_hosts
+    compose_init
+    sed -i "s/hosts_here/$hosts_here/g" $phish_infra_dir/docker-compose.yml
+    compose_up
+}
+
+evilginx() {   
     hostname=$1
     subdomains=$2
     tracker=$3.$1
-
-    for i in ${!subdomains[@]}
-    do
-        subdomains[i]=${subdomains[i]}.$hostname
-    done
-
-    echo "Hostname: $hostname"
-    echo "Subdomains: ${subdomains[@]}"
-    echo "Tracker: $tracker"
-    hosts_here[0]=$hostname
-    printf -v joined '%s,' "${hosts_here[@]}"
-    hosts_here=`echo "${joined%,}"`
-
-    cp $phish_infra_dir/docker-compose.template $phish_infra_dir/docker-compose.yml
+    gen_certs
+    calc_hosts
+    compose_init
     sed -i "s/hosts_here/$tracker/g" $phish_infra_dir/docker-compose.yml
     sed -i "s/hosts_here/$hosts_here/g" $phish_infra_dir/evilginx/evilginx
-
-    cd $phish_infra_dir
-    docker-compose up -d
-
-
-    cat <<EOF >/usr/local/bin/evilginx
-#!/bin/bash
-cd /opt/phish-infra/evilginx
-bash evilginx
-EOF
+    compose_up
     chmod +x /usr/local/bin/evilginx
-
-    tmux new-session -d -s evilginx && tmux send-keys -t evilginx "evilginx" Enter
-
+    tmux new-session -d -s evilginx && tmux send-keys -t evilginx "cd $phish_infra_dir/evilginx ./evilginx" Enter
 }
 
 while getopts 'm:h:s:t:' flag; do
@@ -119,24 +92,15 @@ while getopts 'm:h:s:t:' flag; do
 done
 
 case $mode in
-
-      setup)
-        setup
-        ;;
-
       gophish)
         gophish $hostname ${subdomains[@]}
         ;;
-
       evilginx)
         evilginx $hostname $subdomains $tracker
         ;;
-
       *)
         usage
         ;;
-
-    esac
-
+esac
 
 echo "Done!"
