1ed4398b5408e43976c2974e6bc8d9cce8ee0fd2
lucidafx <89973739+lucidafx@users.noreply.github.com>
2021-09-07 15:25:27 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 phish_infra_dir=/opt/phish_infra
 dockerhub_username="name"
-github_repo_url="repo"
+github_repo_url="https://github.com/shlok-lucideus/phish_infra"
 packages="openssl python3-certbot-nginx python3-pip unzip tmux git docker docker.io docker-compose certbot net-tools iputils-ping iproute2 curl wget nano"
 DEBIAN_FRONTEND=noninteractive apt-get -y update && apt-get -y dist-upgrade && apt-get install -y $packages && apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/*
 for image in evilginx gophish nginx
